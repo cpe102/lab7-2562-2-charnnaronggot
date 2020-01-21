a0611b308e22710cf4729422041f0ae8181395ff
charnnaronggot <charnnarong_2542@hotmail.com>
2020-01-21 16:49:48 +0700

    TEST 7

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,7 +3,25 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(char a ){
+
+	 if( a > 90)
+		a = 'A';
+	else if (a > 75 )
+		a = 'B';
+	else if  (a > 60 )
+		a = 'C';
+	else if (a > 45)
+		a = 'D';
+	else  
+		a = 'F'; 
+	
+	return a ;
+}
+
+	
+	
+
 
 int main(){
 	//Input the number of students
@@ -17,9 +35,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin , name[i]) ;
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i] ;
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,72 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+ using namespace std;
+ #include<string>
+
+ int main()
+ {
+     int N;
+     cout << "Enter your age : ";
+     cin  >> N;
+     if(N <=20)
+     {
+             int H;
+             cout << "Enter your Height:";
+             cin  >> H;
+
+             { 
+                 if(H<=160)
+                     {
+                         cout <<"Status: =  UNFRIEND";
+                     }   
+                 else
+                     {
+                         if(H < 175)
+                             {
+                                 cout <<"Status: =  FRIEND";  
+                             }
+                         else
+                         {
+                             int M;
+                             cout <<"Enter your  Money:";
+                             cin  >> M;
+                             if(M>=69000000)
+                             {
+                                 cout <<"Status: = MARRIED";
+                             }else
+                             {
+                                 cout <<"Status: = ONE-NIGHT-STAND";
+                             }
+
+
+                         }
+
+
+
+
+                     }           
+             }
+
+     }else
+     {
+         if(N <30)
+         {
+            int L;
+            cout <<"Enter Legacy:";
+            cin  >>L;
+            {if(L>10000000)
+            {
+             cout <<"Status: = BEST FRIEND";
+            }else
+            {
+             cout <<"Status: = UNFRIEND";
+            }
+            }
+         }else
+         {
+             cout << "Status: = UNFRIEND";
+         }
+
+     }
+
+     return 0;
+ }
